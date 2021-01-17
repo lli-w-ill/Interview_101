6a4371fd4f06ef61fca16eb04f9cfed448d46e73
galaxie <will.yxlee@gmail.com>
2021-01-16 21:27:40 -0500

    Hash Table, Stack, Tree
    Hash Table, Stack, Tree

---
+++ Algorithms/leetcode/EX_94_Binary_Tree_Inorder_Traversal/Soln_2_iteration.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ans = []
+        currNode = root
+        while currNode:
+            if currNode.left:
+                # 将 currNode 成为 currNode 左子树的“最右节点”的右孩子
+                rightmost = currNode.left
+                while rightmost.right:
+                    rightmost = rightmost.right
+                rightmost.right = currNode
+                # 并且断开 currNode 和 currNode 的左孩子
+                temp = currNode
+                currNode = currNode.left
+                temp.left = None
+            else:
+                ans.append(currNode.val)
+                currNode = currNode.right
+        return ans
