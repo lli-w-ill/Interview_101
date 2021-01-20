3730b9398cff52dfee55d9e90b68fa41ecc950a1
galaxie <will.yxlee@gmail.com>
2021-01-20 18:31:12 -0500

    Tree, Breadth-first Search
    Tree, Breadth-first Search

---
+++ Algorithms/leetcode/Ex_107_Binary_Tree_Level_Order_Traversal_II/Soln_1_Bfs.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        levelOrder = []
+        if root is None:
+            return levelOrder
+        currLevel = [root]
+        while currLevel:
+            nextLevel = []
+            levelOrder.append([node.val for node in currLevel])
+            while currLevel:
+                node = currLevel.pop(0)
+                if node.left:
+                    nextLevel.append(node.left)
+                if node.right:
+                    nextLevel.append(node.right)
+            currLevel = nextLevel
+        return levelOrder[::-1]
