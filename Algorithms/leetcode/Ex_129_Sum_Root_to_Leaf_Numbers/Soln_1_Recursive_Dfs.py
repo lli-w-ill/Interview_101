c78d88b42730d137485c70c58e6f7bc03b92beb1
galaxie <will.yxlee@gmail.com>
2021-01-29 19:29:13 -0500

    Tree, Depth-first Search
    Tree, Depth-first Search

---
+++ Algorithms/leetcode/Ex_129_Sum_Root_to_Leaf_Numbers/Soln_1_Recursive_Dfs.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        ssum = [0]
+        self.dfs(root, 0, ssum)
+        return ssum[0]
+
+    def dfs(self, node: TreeNode, curr: int, ssum: List[int]) -> None:
+        # base cases
+        if not node:
+            return
+        if not node.left and not node.right:
+            ssum[0] += curr * 10 + node.val
+            return
+
+        if node.left:
+            self.dfs(node.left, curr * 10 + node.val, ssum)
+        if node.right:
+            self.dfs(node.right, curr * 10 + node.val, ssum)
---
+++ Algorithms/leetcode/Ex_129_Sum_Root_to_Leaf_Numbers/Soln_2_Iterative_Dfs.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        ssum = 0
+        stack = [(root, 0)]
+        while stack:
+            node, num = stack.pop(0)
+            if not node:
+                return num
+            if node.left:
+                stack.append((node.left, num * 10 + node.val))
+            if node.right:
+                stack.append((node.right, num * 10 + node.val))
+            if not node.left and not node.right:
+                ssum += num * 10 + node.val
+        return ssum
