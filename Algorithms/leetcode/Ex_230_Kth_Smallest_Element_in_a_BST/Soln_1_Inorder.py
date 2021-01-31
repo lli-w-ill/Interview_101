ab9002bbdb75675fe4702e7cb4464cf960c49ce2
galaxie <will.yxlee@gmail.com>
2021-01-31 16:41:06 -0500

    Binary Search, Tree
    Binary Search, Tree

---
+++ Algorithms/leetcode/Ex_230_Kth_Smallest_Element_in_a_BST/Soln_1_Inorder.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        return self.inorder(root, k)[0].val
+
+    def inorder(self, node: TreeNode, k: int) -> Tuple[TreeNode, int]:
+        if node.left:
+            left, k = self.inorder(node.left, k)
+            if k == 0:
+                return left, k
+        k -= 1
+        if k == 0:
+            return node, k
+        if node.right:
+            right, k = self.inorder(node.right, k)
+            if k == 0:
+                return right, k
+        return node, k
---
+++ Algorithms/leetcode/Ex_230_Kth_Smallest_Element_in_a_BST/Soln_2_Iteration.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        if not root:
+            return 0
+        stack = []
+        node = root
+        while True:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            k -= 1
+            if k == 0:
+                return node.val
+            node = node.right
+
+
+# 参考：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/solution/er-cha-sou-suo-shu-zhong-di-kxiao-de-yuan-su-by-le/
