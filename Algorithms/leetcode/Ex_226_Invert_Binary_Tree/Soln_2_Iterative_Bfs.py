1b7e9a0795a7a336907e616cd2d7822c5b489e13
galaxie <will.yxlee@gmail.com>
2021-01-31 15:54:57 -0500

    Tree
    Tree

---
+++ Algorithms/leetcode/Ex_226_Invert_Binary_Tree/Soln_2_Iterative_Bfs.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        queue = [root]
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                node.left, node.right = node.right, node.left
+        return root
