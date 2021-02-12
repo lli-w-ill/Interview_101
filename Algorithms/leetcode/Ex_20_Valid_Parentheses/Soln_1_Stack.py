d2a8915bf0db59d0ab1b6ec17389cdbc0702d9bb
galaxie <will.yxlee@gmail.com>
2021-02-12 14:35:58 -0500

    Array, Stack
    Array, Stack

---
+++ Algorithms/leetcode/Ex_20_Valid_Parentheses/Soln_1_Stack.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for ch in s:
+            if ch == '(' or ch == '{' or ch == '[':
+                stack.append(ch)
+            elif ch == ')':
+                if not stack or stack[-1] != '(':
+                    return False
+                else:
+                    stack.pop()
+            elif ch == ']':
+                if not stack or stack[-1] != '[':
+                    return False
+                else:
+                    stack.pop()
+            elif ch == '}':
+                if not stack or stack[-1] != '{':
+                    return False
+                else:
+                    stack.pop()
+            else:
+                return False
+        return len(stack) == 0
