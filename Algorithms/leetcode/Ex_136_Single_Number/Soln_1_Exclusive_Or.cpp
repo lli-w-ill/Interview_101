30dc494cd5dda87404419191ddefc82a947d9d39
galaxie <will.yxlee@gmail.com>
2021-02-20 07:27:22 -0500

    Hash Table, Bit Manipulation
    Hash Table, Bit Manipulation

---
+++ Algorithms/leetcode/Ex_136_Single_Number/Soln_1_Exclusive_Or.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution_1 {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+};
