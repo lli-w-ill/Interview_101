056471cd7b6f39bdde632fc58098f355e9d57a70
galaxie <will.yxlee@gmail.com>
2021-02-24 13:35:42 -0500

    helloworld
    helloworld

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
 
 int main() {
     std::cout << "hello world" << std::endl;
+
+    vector<char> hello {'h', 'e', 'l', 'l', 'o'};
+    vector<vector<char> > hw {
+        {'h', 'e', 'l', 'l', 'o'},
+        {'w', 'o', 'r', 'l', 'd'}
+    };
+    for (char c : hello) {
+        cout << c;
+    }
+    cout << endl;
+    for (auto vec : hw) {
+        for (auto c : vec) {
+            cout << c;
+        }
+        cout << " ";
+    }
+    cout << endl;
+
     return 0;
 }
