6ebdaf2f5876c60943c8a4420ffcf50d0c46f462
galaxie <will.yxlee@gmail.com>
2021-03-25 17:01:46 -0400

    Factory Pattern and Abstract Factory Pattern
    Factory Pattern and Abstract Factory Pattern

--- DesignPatterns/Chapter02_Creational_Design_Patterns/P2_Factory_Pattern/Version1_Factory_Pattern/Example1.java
+++ DesignPatterns/Chapter02_Creational_Design_Patterns/P2_Factory_Pattern/Version1_Factory_Pattern/Example1.java
@@ -5,7 +5,7 @@ public class Example1 {
     public static void main(String[] args) {
         
         // This is bad
-        Animal bad = new Cat();
+//        Animal bad = new Cat();
         // because if in the future we want to change AmericanCat, we need to change the line to `Animal bad = new AmericanCat();`
         // but if we use factory, we only need to add an enum and change the line to `Animal cat = AnimalFactory.getAnimal(AnimalType.AmericanCat);`
 
