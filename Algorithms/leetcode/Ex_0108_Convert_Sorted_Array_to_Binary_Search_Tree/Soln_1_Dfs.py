551eb79d4947eb32a23b184a90a5e75a30f3bac4
galaxie <will.yxlee@gmail.com>
2021-04-06 14:09:59 -0400

    Tree, Depth-first Search

--- Algorithms/leetcode/Ex_0108_Convert_Sorted_Array_to_Binary_Search_Tree/Soln_1_Dfs.py
+++ Algorithms/leetcode/Ex_0108_Convert_Sorted_Array_to_Binary_Search_Tree/Soln_1_Dfs.py
@@ -1,10 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution_1:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
         if not nums:
             return None
