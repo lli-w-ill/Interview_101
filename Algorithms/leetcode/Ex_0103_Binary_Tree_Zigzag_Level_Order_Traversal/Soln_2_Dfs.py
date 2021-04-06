2afca1898ab23166f6eb04bdd89734eba283d549
galaxie <will.yxlee@gmail.com>
2021-04-06 14:05:47 -0400

    Stack, Tree, Breadth-first Search

--- Algorithms/leetcode/Ex_0103_Binary_Tree_Zigzag_Level_Order_Traversal/Soln_2_Dfs.py
+++ Algorithms/leetcode/Ex_0103_Binary_Tree_Zigzag_Level_Order_Traversal/Soln_2_Dfs.py
@@ -5,7 +5,7 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution_2:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
         if root is None:
             return []
