5c412a196e75cb6bd5237cb68f50749567b84d91
galaxie <will.yxlee@gmail.com>
2021-04-06 14:08:53 -0400

    Array, Tree, Depth-first Search

--- Algorithms/leetcode/Ex_0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Soln_1_Recursion.py
+++ Algorithms/leetcode/Ex_0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Soln_1_Recursion.py
@@ -1,10 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution_1:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         if not inorder or not postorder:
             return None
