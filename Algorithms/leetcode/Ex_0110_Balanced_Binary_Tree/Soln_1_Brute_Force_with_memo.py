2242fa43c8a39bb55f2bcc6a0faa239dcd5afacb
galaxie <will.yxlee@gmail.com>
2021-04-06 14:11:00 -0400

    Tree, Depth-first Search, Recursion

--- Algorithms/leetcode/Ex_0110_Balanced_Binary_Tree/Soln_1_Brute_Force_with_memo.py
+++ Algorithms/leetcode/Ex_0110_Balanced_Binary_Tree/Soln_1_Brute_Force_with_memo.py
@@ -4,7 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+
+class Solution_1:
     def isBalanced(self, root: TreeNode) -> bool:
         if not root:
             return True
