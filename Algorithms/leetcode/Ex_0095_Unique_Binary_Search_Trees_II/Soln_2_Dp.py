06e20078632276eb86c6a7f7356bd6054c787f85
galaxie <will.yxlee@gmail.com>
2021-04-06 14:00:54 -0400

    Dynamic Programming, Tree

--- Algorithms/leetcode/Ex_0095_Unique_Binary_Search_Trees_II/Soln_2_Dp.py
+++ Algorithms/leetcode/Ex_0095_Unique_Binary_Search_Trees_II/Soln_2_Dp.py
@@ -1,13 +1,14 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 from collections import defaultdict
 
-
-class Solution:
+class Solution_2:
     def generateTrees(self, n: int) -> List[TreeNode]:
         if n == 0:
             return []
