7a4e346e5353282f94eb8d7d1bf137277c2e19ae
galaxie <will.yxlee@gmail.com>
2021-04-08 14:10:08 -0400

    Array, Greedy

--- Algorithms/leetcode/Ex_122_Best_Time_to_Buy_and_Sell_Stock_II/Soln_1_Greedy.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if not prices or len(prices) == 1:
-            return 0
-        profit = 0
-        for i in range(len(prices)-1):
-            if prices[i+1] > prices[i]:
-                profit += (prices[i+1] - prices[i])
-        return profit
