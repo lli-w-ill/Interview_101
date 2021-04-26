d105870ff455d6f19637dbc431530e55c4a9ecb4
galaxie <will.yxlee@gmail.com>
2021-04-26 17:22:58 -0400

    helloworld

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <set>
-
-using std::vector;
+  
+using std::cin;
 using std::cout;
 using std::endl;
 
 int main() {
-    std::set<std::pair<std::string, int>> set = {
-        {"A", 4}, {"B", 4}, {"C", 1}, {"A", 0}, {"B", 4}
-    };
- 
-    for (auto const &p: set) {
-        std::cout << p.first << " " << p.second << '\n';
-    }
-
-
+    int num;
+    cin >> num;
+    cout << num << endl;
+    cout << "Hello World" << endl;
     return 0;
 }
